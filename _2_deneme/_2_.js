643b0fa21ce16efbe46b5b12fc0605af8cc7f0be
ffurkanyigit <f.furkanyigit@gmail.com>
2023-06-18 21:53:35 +0300

    revize_f

--- _2_deneme/_2_.js
+++ _2_deneme/_2_.js
@@ -1,25 +1,36 @@
 let value;
-const programmer={
-    name:"Furkan Yiğit",
-    age:28,
-    email:"f.furkanyigit@gmail.com",
-    langs:["Python","Javascript","C++"],
-    adress:{
-        city:"Bursa",
-        street:"Güneştepe"
+const programmer = {
+    name: "Furkan Yiğit",
+    age: 28,
+    email: "f.furkanyigit@gmail.com",
+    langs: ["Python", "Javascript", "C++"],
+    adress: {
+        city: "Bursa",
+        street: "Güneştepe"
     },
-    work:function(){
+    work: function () {
         console.log("Programcı çalışıyor...");
     }
 }
 // bütün verileri çekelim.
-value=programmer;
+value = programmer;
 console.log(value);
 // email verisini çekelim.
-value=programmer.email;
+value = programmer.email;
 console.log(value);
 // city verisini çekelim.
-value=programmer.adress.city;
+value = programmer.adress.city;
 console.log(value);
 // work verisini çekelim.
 programmer.work();
+
+// Eğer bir object literal içinde birden fazla eleman varsa?
+const team=[
+    {name:"Furkan Yiğit",age:28},
+    {name:"Gürkan Yiğit",age:26},
+    {name:"Orhan Yiğit",age:24}
+];
+value=team[0];
+console.log(value);
+value=team[1].name;
+console.log(value);
